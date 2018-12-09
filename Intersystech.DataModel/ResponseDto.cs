17d5faf0ab9633d60d7e0359f0bbeeceaa60a4ca
佐藤秀明 <45691160+sato-hideaki@users.noreply.github.com>
2018-12-09 11:22:02 +0900

    Add files via upload

---
+++ Intersystech.DataModel/ResponseDto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace Intersystech.DataModel
+{
+    /// <summary>
+    /// レスポンスDtoクラス
+    /// </summary>
+    [Serializable]
+    public class ResponseDto : Dto
+    {
+        private DataSet resultSet = new DataSet();
+
+        /// <summary>
+        /// 実行結果データセット
+        /// </summary>
+        public DataSet AffectedResultSet
+        {
+            get
+            {
+                return this.resultSet;
+            }
+            set
+            {
+                this.resultSet = value;
+            }
+        }
+
+        /// <summary>
+        /// 実行結果オブジェクト
+        /// </summary>
+        public object AffectedObject
+        {
+            get;
+            set;
+        }
+
+        private int affectedCount = 0;
+        /// <summary>
+        /// 実行結果件数
+        /// </summary>
+        public int AffectedCount
+        {
+            set
+            {
+                this.affectedCount = value;
+            }
+            get
+            {
+                return this.affectedCount;
+            }
+        }
+
+        private DateTime localDatetime = DateTime.Now;
+        /// <summary>
+        /// システム日時
+        /// </summary>
+        public DateTime SystemDatetime
+        {
+            get
+            {
+                return localDatetime;
+            }
+        }
+    }
+}
---
+++ Intersystech.DataModel/UserInfoBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Intersystech.DataModel
+{
+    /// <summary>
+    /// ユーザ情報クラス
+    /// </summary>
+    [Serializable]
+    public class UserInfoBase
+    {
+        private string id;
+        /// <summary>
+        /// ユーザID
+        /// </summary>
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        private string name;
+        /// <summary>
+        /// ユーザ名
+        /// </summary>
+        public string Name {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
